3890368463a56b9863b1b24b2f0d54fb1a29dbba
Jardas33 <moreiraquim@gmail.com>
2025-09-14 12:37:06 -0400

    Add comprehensive socket event debugging to track turnChanged event issue

--- public/js/go-fish.js
+++ public/js/go-fish.js
@@ -773,17 +773,36 @@ class GoFishClient {
         
         socket.on('goFish', (data) => {
             console.log('🔍 goFish event received on client!', data);
+            console.log('🔍 goFish data:', JSON.stringify(data, null, 2));
             this.updateGoFish(data);
         });
         
         socket.on('turnChanged', (data) => {
             console.log('🔍 turnChanged event received on client!', data);
+            console.log('🔍 turnChanged data:', JSON.stringify(data, null, 2));
             this.updateTurnChanged(data);
         });
         
         // Debug socket connection
         console.log('🔍 Socket connected:', socket.connected);
         console.log('🔍 Socket ID:', socket.id);
+        
+        // Debug: Listen to all socket events
+        const originalEmit = socket.emit;
+        socket.emit = function(...args) {
+            console.log('🔍 Client emitting:', args[0], args[1]);
+            return originalEmit.apply(this, args);
+        };
+        
+        // Debug: Log all incoming events
+        const originalOn = socket.on;
+        socket.on = function(event, callback) {
+            const wrappedCallback = function(...args) {
+                console.log('🔍 Client received event:', event, args[0]);
+                return callback.apply(this, args);
+            };
+            return originalOn.call(this, event, wrappedCallback);
+        };
         
         socket.on('gameOver', (data) => {
             this.showGameOver(data);
